eeccced68be974e15d4ef13d55f8c35bb26ec500
SteviaTrisha <135150942+Sobig-F@users.noreply.github.com>
2026-09-04 01:57:59 +0300

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.go
@@ -0,0 +1,23 @@
+func twoSum(numbers []int, target int) []int {
+	result := make([]int, 0, 2)
+	
+	left := 0
+	right := len(numbers) - 1
+
+	for {
+		if numbers[right] + numbers[left] > target {
+			right--
+		}
+
+		if numbers[right] + numbers[left] < target {
+			left++
+		}
+		if numbers[right] + numbers[left] == target {
+			result = append(result, left + 1, right + 1)
+			return result
+		}
+
+	}
+
+	return result
+}
