0f2e39460986d19e87f57354ca189ecdc7c26916
SteviaTrisha <135150942+Sobig-F@users.noreply.github.com>
2026-09-04 04:43:33 +0300

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.go
@@ -0,0 +1,15 @@
+func dailyTemperatures(temperatures []int) []int {
+	result := make([]int, len(temperatures))
+	stack := make([]int, 0, len(temperatures))
+
+	for i, temp := range temperatures {
+		for len(stack) > 0 && temperatures[stack[len(stack) - 1]] < temp {
+			prevIdx := stack[len(stack) - 1]
+			stack = stack[:len(stack) - 1]
+			result[prevIdx] = i - prevIdx
+		}
+		stack = append(stack, i)
+	}
+
+	return result
+}
